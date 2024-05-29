0e8783299ed2a52e594d4ad4f8da5512ee6ef47f
mcasto-09 <mgcastorani@gmail.com>
2024-05-28 21:22:27 -0600

    step 3

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import playerData from './Playerdata.js'
+
+
+function BaseballCard(props) {
+  // return (
+    // <div className="card">
+      // <h2>{props.name}</h2>
+      // <img src={props.imgUrl} alt={props.name} />
+    // </div>
+  // ); // when return is uncommented this will work
+  const statsDisplay = Object.entries(props.stats).map(([statName, statValue]) => (
+    <p key={statName}>
+      {statName}: {statValue}
+    </p>
+  ));
+  return (
+    <div className="card">
+      <h2>{props.name}</h2>
+      <p>Team: {props.team}</p>
+      <p>Position: {props.position}</p>
+      {statsDisplay}
+    </div>
+  );
+}
+
+function App() {
+  const cards = playerData.map((player) => 
+    <BaseballCard
+      name={player.name}
+      team={player.team}
+      position={player.position}
+      stats={player.stats}
+      imgUrl={player.imgUrl}
+      key={player.cardId}
+    />
+  );
+
+  return <>{cards}</>
+}
+
+export default App;
